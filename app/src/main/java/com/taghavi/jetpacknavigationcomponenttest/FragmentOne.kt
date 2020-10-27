4f64f031394316c5c8e51d5e1842d68b47a5efff
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-27 23:16:13 +0330

    Add destinations and paths in nav_graph

--- app/src/main/java/com/taghavi/jetpacknavigationcomponenttest/FragmentOne.kt
+++ app/src/main/java/com/taghavi/jetpacknavigationcomponenttest/FragmentOne.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_one.*
 
 class FragmentOne : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_one, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_one, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentOneText.setOnClickListener{
+            view.findNavController().navigate(R.id.action_fragmentOne_to_fragmentTwo)
+        }
     }
 }
