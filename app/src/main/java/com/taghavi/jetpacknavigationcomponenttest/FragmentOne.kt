68ce895d27b0c10e83007de5aa3ae39b7b759ddb
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-28 22:33:13 +0330

    adding popUp property to FragmentOne

--- app/src/main/java/com/taghavi/jetpacknavigationcomponenttest/FragmentOne.kt
+++ app/src/main/java/com/taghavi/jetpacknavigationcomponenttest/FragmentOne.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.CallSuper
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_one.*
 
@@ -19,8 +20,12 @@ class FragmentOne : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentOneText.setOnClickListener{
-            view.findNavController().navigate(R.id.action_fragmentOne_to_fragmentTwo)
+        fragmentOneText.setOnClickListener {
+            view.findNavController().navigate(
+                R.id.action_fragmentOne_to_fragmentTwo,
+                null,
+                NavOptions.Builder().setPopUpTo(R.id.fragmentOne, true).build()
+            )
         }
     }
 }
